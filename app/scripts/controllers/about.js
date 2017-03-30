79203de012a9e2817f75dc9eb90b425ee31cb8cf
David <david0718@qq.com>
2017-03-30 12:52:31 +0800

    setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pvApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pvApp
+ */
+angular.module('pvApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pvApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pvApp
+ */
+angular.module('pvApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
